8e9c649e4b0bb17b7f3aff3e08fcae8bf6880f46
Danielx2003 <ahipsterwalrus4@gmail.com>
2025-12-23 12:21:44 +0000

    removed comments

--- src/backend/pubfinder/accounts/views.py
+++ src/backend/pubfinder/accounts/views.py
@@ -5,10 +5,6 @@ from rest_framework import status
 from accounts.models import Pub
 from accounts.serializers import PubSerializer
 
-# class PubCreate(generics.ListCreateAPIView):
-#     queryset = Pub.objects.all()
-#     serializer_class = PubSerializer
-
 class PubListView(APIView):
     def get(self, request):
         pubs = Pub.objects.all()
