bdbce3d227493d07bac45e4ba4790c98adf8d00a
Danielx2003 <ahipsterwalrus4@gmail.com>
2025-12-24 13:52:48 +0000

    new commit

--- src/backend/pubfinder/pub_events/views.py
+++ src/backend/pubfinder/pub_events/views.py
@@ -12,7 +12,6 @@ class PubEventListView(APIView):
         pub_events = PubEvent.objects.all()
 
 
-
         serializer = PubEventSerializer(pub_events, many=True)
 
         return Response(serializer.data, status=status.HTTP_200_OK)
