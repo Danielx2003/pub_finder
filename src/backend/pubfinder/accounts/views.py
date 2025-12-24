d930f56364b831434be09e78ce5216214de7d396
Danielx2003 <ahipsterwalrus4@gmail.com>
2025-12-24 16:17:57 +0000

    commit

--- src/backend/pubfinder/accounts/views.py
+++ src/backend/pubfinder/accounts/views.py
@@ -1,6 +1,7 @@
 """File for API Endpoints"""
 # pylint: disable=missing-function-docstring
 
+
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
