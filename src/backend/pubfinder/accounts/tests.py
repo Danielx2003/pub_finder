ab1af07622e2ab6be93a0fefb0497f143ad41f51
Danielx2003 <ahipsterwalrus4@gmail.com>
2025-12-25 16:54:39 +0000

    fixed linter failings

--- src/backend/pubfinder/accounts/tests.py
+++ src/backend/pubfinder/accounts/tests.py
@@ -1,10 +1,15 @@
+"""Unit Tests for API Endpoints of Accounts app"""
+
 from rest_framework.test import APITestCase
-from django.urls import reverse
 from rest_framework import status
+from django.urls import reverse
+
 from accounts.models import Pub
 
 class PubAPITest(APITestCase):
+    """TestCase for Pub"""
     def test_valid_post_request_returns_ok(self):
+        """Test Post endpoint returns 201 for valid body"""
         url = reverse("Pub-view-create")
         payload = {
             "name": "The Green Man",
@@ -17,6 +22,7 @@ class PubAPITest(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
     def test_get_all_pubs_returns_ok(self):
+        """Test Get endpoint for all pubs returns 200"""
         url = reverse("Pub-view-list")
 
         response = self.client.get(url)
@@ -24,6 +30,7 @@ class PubAPITest(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_create_pub_missing_name_returns_400(self):
+        """Test request with invalid payload body returns 400"""
         url = reverse("Pub-view-create")
         payload = {
             "latitude": 40.741895,
@@ -36,6 +43,7 @@ class PubAPITest(APITestCase):
         self.assertIn("name", response.data)
 
     def test_create_pub_persists_to_db(self):
+        """Test created Pub is stored in DB"""
         url = reverse("Pub-view-create")
         payload = {
             "name": "The Green Man",
@@ -43,8 +51,7 @@ class PubAPITest(APITestCase):
             "longitude": -73.989308,
         }
 
-        response = self.client.post(url, payload, format="json")
+        _ = self.client.post(url, payload, format="json")
 
         self.assertEqual(Pub.objects.count(), 1)
         self.assertTrue(Pub.objects.filter(name="The Green Man").exists())
-
