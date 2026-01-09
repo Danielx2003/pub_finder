0f3dcb64dd5e40e81e3624c328d94c87e130f132
Danielx2003 <ahipsterwalrus4@gmail.com>
2026-01-09 19:11:24 +0000

    refactor(pub): updated all url names

--- src/backend/pubfinder/accounts/urls.py
+++ src/backend/pubfinder/accounts/urls.py
@@ -5,6 +5,7 @@ from . import views
 
 
 urlpatterns = [
-    path("", views.PubListView.as_view(), name="Pub-view-list"),
-    path("/create", views.PubCreateView.as_view(), name="Pub-view-create")
+    path("", views.PubListView.as_view(), name="get-pubs"),
+    path("create", views.PubCreateView.as_view(), name="create-pub"),
+    path("<int:pub_id>", views.PubGetByIdView.as_view(), name="get-pub-by-id")
 ]
