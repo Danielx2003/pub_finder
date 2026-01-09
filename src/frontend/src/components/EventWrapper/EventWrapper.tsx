eb9524baf829dfcdc019d264841ff4914bdbdef3
Danielx2003 <ahipsterwalrus4@gmail.com>
2026-01-09 16:38:22 +0000

    style(events): added styles to Event component

---
+++ src/frontend/src/components/EventWrapper/EventWrapper.tsx
@@ -0,0 +1,31 @@
+import { useParams } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+
+import { useShowEvent } from "../../hooks/events/useShowEvent";
+import Event from '../Event/Event'
+
+export default function EventWrapper() {
+    const navigate = useNavigate();
+
+    let { id } = useParams() as { id: string };
+
+    const { event, loading, error } = useShowEvent({
+        id
+    })
+
+    if (error)
+    {
+        navigate('/')
+    }
+
+    return (
+        <>
+            {loading && <p className="status-message">Loading…</p>}
+            {event && 
+                <Event 
+                    name={event.name}
+                    datetime={new Date(event.date_time)}
+                />}
+        </>
+    )
+}
