65bfc58857f15065d10d89379b2b0a7450b9a99d
Dan <70431670+Danielx2003@users.noreply.github.com>
2026-01-09 14:20:28 +0000

    refactor(events): updated urls, added tests for get-event-by-id (#37)
    * feat(show-event): added tests for get-event-by-id url
    
    * refactor(event-urls): updated url names
    
    * refactor(pubfinder-urls): added / to the end of each url

--- src/backend/pubfinder/events/tests.py
+++ src/backend/pubfinder/events/tests.py
@@ -10,7 +10,7 @@ class EventAPITest(APITestCase):
     """TestCase for Event"""
     def test_valid_post_request_returns_ok(self):
         """Test Post endpoint returns 201 for valid body"""
-        url = reverse("Event-view-create")
+        url = reverse("create-event")
         payload = {
             "name": "Fight",
             "date_time": "2025-12-25T12:00",
@@ -23,7 +23,7 @@ class EventAPITest(APITestCase):
 
     def test_get_all_events_returns_ok(self):
         """Test Get endpoint for all pubs returns 200"""
-        url = reverse("Event-view-list")
+        url = reverse("get-all-events")
 
         response = self.client.get(url)
 
@@ -31,7 +31,7 @@ class EventAPITest(APITestCase):
 
     def test_create_event_missing_name_returns_400(self):
         """Test request with invalid payload body returns 400"""
-        url = reverse("Event-view-create")
+        url = reverse("create-event")
         payload = {
             "date_time": "2025-12-25T12:00",
         }
@@ -43,7 +43,7 @@ class EventAPITest(APITestCase):
 
     def test_create_event_persists_to_db(self):
         """Test created Pub is stored in DB"""
-        url = reverse("Event-view-create")
+        url = reverse("create-event")
         payload = {
             "name": "Boxing Match 1",
             "date_time": "2025-12-25T12:00",
@@ -54,3 +54,33 @@ class EventAPITest(APITestCase):
 
         self.assertEqual(Event.objects.count(), 1)
         self.assertTrue(Event.objects.filter(name="Boxing Match 1").exists())
+
+    def test_get_event_by_id_returns_ok(self):
+        """Test get-event-by-id returns ok"""
+
+        # Create event
+        url = reverse("create-event")
+        payload = {
+            "name": "Boxing Match 1",
+            "date_time": "2025-12-25T12:00",
+            "sport": "BOXING"
+        }
+
+        create_response = self.client.post(url, payload, format="json")
+
+        # Get event with that id
+        url = reverse("get-event-by-id",  kwargs={'event_id':create_response.data['id']})
+
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, 200)
+
+    def test_get_event_by_id_with_invalid_id_returns_404(self):
+        """Test get-event-by-id with invalid id returns 404"""
+
+        # Get event with that id
+        url = reverse("get-event-by-id",  kwargs={'event_id': 1})
+
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, 404)
