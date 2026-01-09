4cf8a05b5a8359beb4918f9f58caac2e061cf3a3
Danielx2003 <ahipsterwalrus4@gmail.com>
2026-01-09 16:39:36 +0000

    feat(app): added /events and /events:id routes

--- src/frontend/src/App.tsx
+++ src/frontend/src/App.tsx
@@ -1,20 +1,28 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 import './App.css'
+import EventSearch from './components/EventSearch/EventSearch'
+import EventWrapper from './components/EventWrapper/EventWrapper'
 
-function App() {
+export default function App() {
   return (
     <>
     <BrowserRouter>
       <Routes>
-        <Route 
+        <Route
           path='/'
           element={<h1>Hello World</h1>}
+        />
+        <Route
+          path='/events'
+          element={<EventSearch />}
+        />
+        <Route
+          path='/events/:id'
+          element={<EventWrapper />}
         />
       </Routes>
     </BrowserRouter>
     </>
   )
-}
-
-export default App
+};
