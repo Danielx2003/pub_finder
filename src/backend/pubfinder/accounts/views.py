90f09fb2f9b015cfb0eb33df493735f431cb8ff9
Dan <70431670+Danielx2003@users.noreply.github.com>
2026-01-09 19:15:33 +0000

    feat(pub): created /pub/<int:pub_id> endpoint (#42)
    * feat(pub): added tests for GetPubById, updated reverse urls
    
    * refactor(pub): updated all url names
    
    * feat(pub): added GetPubById endpoint

--- src/backend/pubfinder/accounts/views.py
+++ src/backend/pubfinder/accounts/views.py
@@ -6,6 +6,8 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 
+from django.shortcuts import get_object_or_404
+
 from accounts.models import Pub
 from accounts.serializers import PubSerializer
 
@@ -27,3 +29,10 @@ class PubCreateView(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class PubGetByIdView(APIView):
+    """[GET] Return single Pub by ID"""
+    def get(self, request, pub_id):
+        event = get_object_or_404(Pub, id=pub_id)
+        serializer = PubSerializer(event)
+        return Response(serializer.data, status=status.HTTP_200_OK)
