ce0106c74d73031c8301756c1c01682879233ffb
Danielx2003 <ahipsterwalrus4@gmail.com>
2026-01-09 14:17:03 +0000

    refactor(pubfinder-urls): added / to the end of each url

--- src/backend/pubfinder/pubfinder/urls.py
+++ src/backend/pubfinder/pubfinder/urls.py
@@ -3,7 +3,7 @@
 from django.urls import include, path
 
 urlpatterns = [
-    path("api/v1/pub", include("accounts.urls")),
-    path("api/v1/event", include("events.urls")),
-    path("api/v1/pub-event", include("pub_events.urls")),
+    path("api/v1/pub/", include("accounts.urls")),
+    path("api/v1/event/", include("events.urls")),
+    path("api/v1/pub-event/", include("pub_events.urls")),
 ]
