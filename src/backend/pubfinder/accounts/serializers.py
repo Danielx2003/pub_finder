659692f19853ebc49a3cb58cc1bccac5f329f739
Danielx2003 <ahipsterwalrus4@gmail.com>
2026-01-11 16:06:57 +0000

    feat(pub): updated serializer for location field

--- src/backend/pubfinder/accounts/serializers.py
+++ src/backend/pubfinder/accounts/serializers.py
@@ -1,10 +1,29 @@
 """File for serializers"""
 
 from rest_framework import serializers
+from django.contrib.gis.geos import Point
 from accounts.models import Pub
 
 class PubSerializer(serializers.ModelSerializer):
     """Serializer for Pub object"""
+
+    latitude = serializers.FloatField(write_only=True)
+    longitude = serializers.FloatField(write_only=True)
+    latitude_read = serializers.SerializerMethodField()
+    longitude_read = serializers.SerializerMethodField()
+
     class Meta:
         model = Pub
-        fields = ["id", "name", "longitude", "latitude"]
+        fields = ['id', 'name', 'latitude', 'longitude', 'latitude_read', 'longitude_read']
+
+    def create(self, validated_data):
+        lat = validated_data.pop('latitude')
+        lon = validated_data.pop('longitude')
+        validated_data['location'] = Point(lon, lat)
+        return super().create(validated_data)
+
+    def get_latitude_read(self, obj):
+        return obj.location.y
+
+    def get_longitude_read(self, obj):
+        return obj.location.x
