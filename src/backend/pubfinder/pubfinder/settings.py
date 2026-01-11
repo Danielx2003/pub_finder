f12f4582b305457da8a5a2c95e939b9ec861fdb0
Danielx2003 <ahipsterwalrus4@gmail.com>
2026-01-11 16:07:55 +0000

    feat(db): migrated db from sqllite to postgis

--- src/backend/pubfinder/pubfinder/settings.py
+++ src/backend/pubfinder/pubfinder/settings.py
@@ -29,6 +29,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.gis',
     'corsheaders',
     'accounts',
     'events',
@@ -72,8 +73,11 @@ WSGI_APPLICATION = 'pubfinder.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': 'gisdb',
+        'USER': 'myuser',
+        'PASSWORD': 'pass',
+        'HOST': 'localhost',
     }
 }
 
