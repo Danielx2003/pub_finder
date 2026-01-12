efc4c4439febddee425e76de4d65e2ce17df0608
Danielx2003 <ahipsterwalrus4@gmail.com>
2026-01-12 09:55:51 +0000

    fix(settings): updated host from 'localhost' to 'db'

--- src/backend/pubfinder/pubfinder/settings.py
+++ src/backend/pubfinder/pubfinder/settings.py
@@ -77,10 +77,10 @@ DATABASES = {
         'NAME': 'gisdb',
         'USER': 'myuser',
         'PASSWORD': 'pass',
-        'HOST': 'localhost',
+        'HOST': 'db',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
