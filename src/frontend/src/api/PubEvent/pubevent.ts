5edde6918611adb3cf8b852b0ccf4a19df20f422
Danielx2003 <ahipsterwalrus4@gmail.com>
2026-01-13 22:26:30 +0000

    feat(pubevent): added pagination and location params to api

--- src/frontend/src/api/PubEvent/pubevent.ts
+++ src/frontend/src/api/PubEvent/pubevent.ts
@@ -1,6 +1,10 @@
 type GetPubEventsParams = {
   event_id?: number
   pub_id?: number
+  latitude?: number
+  longitude?: number
+  distance?: number
+  page?: number
 }
 
 export async function getPubEvents(params: GetPubEventsParams) {
@@ -12,8 +16,20 @@ export async function getPubEvents(params: GetPubEventsParams) {
     url.searchParams.append('event_id', params.event_id.toString())
   }
 
-  if (params.pub_id) {
-    url.searchParams.append('pub_id', params.pub_id.toString())
+  if (params.latitude) {
+    url.searchParams.append('latitude', params.latitude.toString())
+  }
+
+  if (params.longitude) {
+    url.searchParams.append('longitude', params.longitude.toString())
+  }
+
+  if (params.distance) {
+    url.searchParams.append('distance', params.distance.toString())
+  }
+
+  if (params.page) {
+    url.searchParams.append('page', params.page.toString())
   }
 
   const res = await fetch(url.toString())
