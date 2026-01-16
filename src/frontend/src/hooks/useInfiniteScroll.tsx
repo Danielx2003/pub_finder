a0d42b4066c8aa3cae4626548cc7c1ea9fe72cab
Dan <70431670+Danielx2003@users.noreply.github.com>
2026-01-16 08:59:19 +0000

    refactor(infinite-scroll): moved infinite scroll to a hook (#57)
    * refactor(event): moved infinite scroll logic to hook
    
    * feat(hook): created useInfiniteScroll hook

---
+++ src/frontend/src/hooks/useInfiniteScroll.tsx
@@ -0,0 +1,43 @@
+import { useEffect, useCallback } from "react";
+
+type UseInfiniteScrollParams = {
+    trigger: React.Dispatch<React.SetStateAction<number>>
+    screenId?: string
+    currentPage: number | undefined
+    maxPage: number | undefined
+    loading: boolean
+}
+
+const useInfiniteScroll = ({
+    trigger,
+    screenId = 'root',
+    currentPage,
+    maxPage,
+    loading
+}: UseInfiniteScrollParams) => {
+    const handleScroll = useCallback(() => {
+        const root = document.getElementById(screenId);
+        if (!root || loading) return;
+
+        const bottom =
+            window.scrollY + window.innerHeight >= root.clientHeight;
+
+        if (bottom &&
+            currentPage &&
+            maxPage &&
+            currentPage + 1 <= maxPage
+        ) {
+            trigger(prev => prev+1);
+        }
+    }, [trigger, screenId, currentPage, maxPage, loading]);
+
+    useEffect(() => {       
+        window.addEventListener('scroll', handleScroll);
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, [handleScroll]);
+}
+
+export default useInfiniteScroll;
