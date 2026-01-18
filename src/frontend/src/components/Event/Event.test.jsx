0a2cd84f859b5be81f91e9199f247dcfe5eb3a1d
Danielx2003 <ahipsterwalrus4@gmail.com>
2026-01-18 19:38:29 +0000

    fix(event): updated unit tests for Link back button

--- src/frontend/src/components/Event/Event.test.jsx
+++ src/frontend/src/components/Event/Event.test.jsx
@@ -1,38 +1,70 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom'
+
 import { render, screen } from '@testing-library/react';
 import Event from './Event';
 import { describe, it, expect } from 'vitest';
 
 describe('Event', () => {
     it('Renders the name', () => {
-        render(<Event
-            id={'1'}
-            name={'Event 1'}
-            datetime={new Date(2024, 2, 10, 2, 30)}
-            />);
+        render(
+            <BrowserRouter>
+                <Event
+                    id={'1'}
+                    name={'Event 1'}
+                    datetime={new Date(2024, 2, 10, 2, 30)}
+                />
+            </BrowserRouter>
+        );
         const name = screen.getByText('Event 1');
         expect(name).toBeInTheDocument();
     })
 
     it('Renders Date and Time', () => {
-        render(<Event
-            id={'1'}
-            name={'Event 1'}
-            datetime={new Date(2024, 2, 10, 2, 30)}
-            />);
+        render(
+            <BrowserRouter>
+                <Event
+                    id={'1'}
+                    name={'Event 1'}
+                    datetime={new Date(2024, 2, 10, 2, 30)}
+                />
+            </BrowserRouter>
+        );
         const datetime = screen.getByText('March 10, 2024');
         expect(datetime).toBeInTheDocument();
     })
 
-    // it('Renders countdown till event', () => {
-    //     let dateNow = new Date()
-    //     dateNow.setFullYear(dateNow.getFullYear() + 1)
+    it('Renders countdown till event', () => {
+        let dateNow = new Date()
+        dateNow.setFullYear(dateNow.getFullYear() + 1)
 
-    //     render(<Event 
-    //         name={'Event 1'}
-    //         datetime={dateNow}
-    //         />);
-    //     const countdown = screen.getByRole('Countdown');
-    //     expect(countdown).toBeInTheDocument();
-    // })
+        render(
+            <BrowserRouter>
+                <Event
+                    id={'1'}
+                    name={'Event 1'}
+                    datetime={new Date(2024, 2, 10, 2, 30)}
+                />
+            </BrowserRouter>
+        );
+        const countdown = screen.getByTestId('countdown');
+        expect(countdown).toBeInTheDocument();
+    })
+
+    it('Renders back button', () => {
+        let dateNow = new Date()
+        dateNow.setFullYear(dateNow.getFullYear() + 1)
+
+        render(
+            <BrowserRouter>
+                <Event
+                    id={'1'}
+                    name={'Event 1'}
+                    datetime={new Date(2024, 2, 10, 2, 30)}
+                />
+            </BrowserRouter>
+        );
+        const countdown = screen.getByText('Back');
+        expect(countdown).toBeInTheDocument();
+    })
 });
